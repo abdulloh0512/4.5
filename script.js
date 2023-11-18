b54914bea95e66a8118b23e58b406d2a14a71365
abdulloh0512 <abdullohdev0512@gmail.com>
2023-11-18 20:23:54 +0500

    4.5

---
+++ script.js
@@ -0,0 +1,99 @@
+// 1-masala
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+
+// sum(+prompt("1-sonni kiriting."), +prompt("2-sonni kiriting."))
+
+// 2-masala
+// function toSecund(minut) {
+//     console.log(minut * 60 + " secund");
+// }
+
+// toSecund(prompt("Minutni kiriting."))
+
+// 3-masala
+// function nextInteger(int) {
+//     console.log(++int)
+// }
+
+// nextInteger(+prompt("Butun son kiriting."))
+
+// 4-masala
+// function uchburchak(asos, balandlik) {
+//   console.log((asos * balandlik) / 2);
+// }
+
+// uchburchak(
+//   +prompt("Uchburchakning asosini kiriting"),
+//   +prompt("Uchburchakning balandligini kiriting.")
+// );
+
+// 5-masala
+// function ageToDays(age) {
+//     console.log(age * 365);
+// }
+
+// ageToDays(+prompt("Yoshingizni kiriting."))
+
+// 6-masala
+// function kub(a) {
+//     console.log(a * a * a);
+// }
+
+// kub(+prompt("Biror butun son yozing."))
+
+// 8-masala
+// function power(x, y) {
+//     console.log(x * y);
+// }
+
+// power(+prompt("x ning o'rniga son kiriting"), +prompt("y ning o'rniga son kiriting."))
+
+// 9-masala
+// function toSecund(hour) {
+//     console.log(hour * 60 * 60);
+// }
+
+// toSecund(+prompt("Soatni kiriting."))
+
+// 10-masala
+// function uchinchiTomon(tomon1, tomon2) {
+//   console.log(tomon1 + tomon2 - 1);
+// }
+
+// uchinchiTomon(
+//   +prompt("Uchburchakning 1-tomonini kiriting."),
+//   +prompt("Uchburchakning 2-tomonini kiriting.")
+// );
+
+// 11-masala
+// function qoldiq(a, b) {
+//     console.log(a % b);
+// }
+
+// qoldiq(+prompt("a sonni kiriting."), +prompt("b sonni kiriting."))
+
+// 12-masala
+// function tortburchakYuzi(a, b) {
+//     console.log(a * b);
+// }
+
+// tortburchakYuzi(
+//   +prompt("To'rtburchakning 1-tomonini kiriting."),
+//   +prompt("To'rtburchakning 2-tomonini kiriting.")
+// );
+
+// 13-masala
+// function stringQoshish(a) {
+//   console.log("Something " + a);
+// }
+
+// stringQoshish(prompt("Biror string qiymat yozing"));
+
+// 14-masala
+// function kvadrat(a) {
+//     console.log(a * a);
+// }
+
+// kvadrat(+prompt("Kvadratning tomonini kiriting."))
